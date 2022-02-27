7233dfdd1a463abdd5f14a64d903c9987207a748
Gian Alves <gian.alves@jrcomercio.com.br>
2022-02-27 18:49:05 -0300

    Adicionando css adicionais na pasta assets do projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas';
+import './assets/App.css';
+import './assets/index.css';
 class App extends Component {
     render() {
         return (
