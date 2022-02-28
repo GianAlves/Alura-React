98a1e393e7a118ac55a58ca9f80b20f11a48fa03
Gian Alves <gian.alves@jrcomercio.com.br>
2022-02-28 01:19:39 -0300

    Colocando icon de delete svg no card

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import './estilo.css';
-
+// import deleteSvg from '../../assets/img/delete.svg';
+import { ReactComponent as DeleteSvg } from '../../assets/img/delete.svg';
 class CardNota extends Component {
-  render() {
-    return (
-      <section className='card-nota'>
-        <header className='card-nota_cabecalho'>
-          <h3 className='card-nota_titulo'>{this.props.titulo}</h3>
-        </header>
-        <p className='card-nota_texto'>{this.props.texto}</p>
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section className='card-nota'>
+                <header className='card-nota_cabecalho'>
+                    <h3 className='card-nota_titulo'>{this.props.titulo}</h3>
+                    {/* Utilizando svg como img ou como um component... */}
+                    {/* <img src={deleteSvg} alt="Delete" /> */}
+                    <DeleteSvg/>
+                </header>
+                <p className='card-nota_texto'>{this.props.texto}</p>
+            </section>
+        );
+    }
 }
 
 export default CardNota;
